b696df3b3c07a16bd6d4764b569e283775f58a09
reuben21 <reuben211999@icloud.com>
2021-02-27 14:23:38 +0530

    Updated Splash Screen Component

--- vaccine-locator-backend/src/db/mongoose.js
+++ vaccine-locator-backend/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/hackerBash-api', {
+mongoose.connect(process.env.MONGO_DB_CONNECTION_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
