f6cf43bf5b8b51671c06c0db8bfa038ed3c23751
reuben21 <reuben211999@icloud.com>
2021-02-27 15:50:46 +0530

    Updated Splash Screen

--- vaccine-locator-frontend/App.js
+++ vaccine-locator-frontend/App.js
@@ -1,7 +1,7 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
-
+import SplashScreen from "./Components/SplashScreen/SplashScreen";
 export default class App extends Component {
     state = {
         isVisible: true
@@ -16,19 +16,12 @@ export default class App extends Component {
     componentDidMount() {
         var that = this;
         setTimeout(function () {
-            that.Hide_Splash_Screen();
+            that.Hide_Splash_Screen()
         }, 5000);
     }
 
     render() {
-        let Splash_Screen = (
-            <View style={styles.SplashScreen_RootView}>
-                <View style={styles.SplashScreen_ChildView}>
-                    <Image
-                        source={{uri: 'https://static.javatpoint.com/tutorial/react-native/images/react-native-tutorial.png'}}
-                        style={{width: '100%', height: '100%', resizeMode: 'contain'}}/>
-                </View>
-            </View>)
+        let Splash_Screen = <SplashScreen/>
         return (
             <View style={styles.MainContainer}>
 
@@ -47,29 +40,18 @@ export default class App extends Component {
 
 const styles = StyleSheet.create(
     {
-        MainContainer:
-            {
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                paddingTop: 20
-            },
+        MainContainer: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
 
-        SplashScreen_RootView:
-            {
-                justifyContent: 'center',
-                flex: 1,
-                margin: 10,
-                position: 'absolute',
-                width: '100%',
-                height: '100%',
-            },
+        },
+
 
         SplashScreen_ChildView:
             {
                 justifyContent: 'center',
                 alignItems: 'center',
-                backgroundColor: '#00BCD4',
-                flex: 1,
-            },
-    });
+                flex: 1
+
+    }});
---
+++ vaccine-locator-frontend/Components/SplashScreen/SplashScreen.js
@@ -0,0 +1,43 @@
+import {StatusBar} from 'expo-status-bar';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import * as colors from '../../Color'
+export default class SplashScreen extends Component {
+
+
+    render() {
+        return (
+            <View style={styles.SplashScreen_RootView}>
+                <View style={styles.SplashScreen_ChildView}>
+                    <Image
+                        source={require('../Images/vaccine.png')}
+                    />
+                    <Text style={{
+                        color:colors.BLUEISH_GREEN
+                    }}>Get you Vaccine</Text>
+                </View>
+            </View>)
+
+    };
+
+}
+
+const styles = StyleSheet.create(
+    {
+        SplashScreen_RootView:
+            {
+                justifyContent: 'center',
+                flex: 1,
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+            },
+
+        SplashScreen_ChildView:
+            {
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: colors.OFF_WHITE,
+                flex: 1,
+            },
+    });
