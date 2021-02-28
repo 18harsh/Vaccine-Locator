c6d6ab64896141572fc68f12cd6875384f6d6611
reuben21 <reuben211999@icloud.com>
2021-02-28 17:43:13 +0530

    Updated UI fro Clinic Sign Up

--- VacccinLocatorFrontend/Pages/VacinationClinic/Signup/Signup.js
+++ VacccinLocatorFrontend/Pages/VacinationClinic/Signup/Signup.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Text, View, Button, Alert, StyleSheet } from "react-native";
+import { Text, View, Button, Alert, StyleSheet, Image } from "react-native";
 import { DefaultTheme, TextInput } from "react-native-paper";
 import * as planted_colors from "../../../Components/Color";
 import { Formik } from "formik";
 import * as yup from "yup";
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { ScrollView } from "react-native-gesture-handler";
+
+navigator.geolocation = require("@react-native-community/geolocation");
+navigator.geolocation = require("react-native-geolocation-service");
 const theme = {
   ...DefaultTheme,
   roundness: 4,
@@ -15,137 +19,151 @@ const theme = {
   },
 };
 
+const homePlace = {
+  description: "Home",
+  geometry: { location: { lat: 48.8152937, lng: 2.4597668 } },
+};
+const workPlace = {
+  description: "Work",
+  geometry: { location: { lat: 48.8496818, lng: 2.2940881 } },
+};
 
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 const loginValidationSchema = yup.object().shape({
-   clinicName: yup
+  clinicName: yup
     .string()
     .required("clinic Name is required"),
   phoneNo: yup.string().matches(phoneRegExp, "Phone number is not valid"),
 });
 
 const MyReactNativeForm = props => (
-  <View style={styles.MainContainer}>
+  <ScrollView>
+    <View style={styles.MainContainer}>
+      <View style={styles.SplashScreen_ChildView}>
+        <Image
+          source={require('../../../Components/Images/clinic.png')}
+        />
+        <Text style={{
+          color: planted_colors.STRONG_RED,
+          fontSize: 15
+        }}>Add your Clinic</Text>
+      </View>
+
+      <Formik
+        validationSchema={loginValidationSchema}
+        initialValues={{
+          clinicName: "",
+          AddressOfClinic: "",
+          ClinicCoordinates: "",
+          phoneNo: "",
+          vaccineCount: "",
+        }}
+        onSubmit={values => {
+          console.log(values);
+          const jsonData = JSON.stringify(values);
+
+        }}
+
+      >
+        {({
+            handleChange, handleBlur, handleSubmit, values, errors,
+            touched,
+            isValid,
+          }) => (
+          <View style={{
+            height: "100%",
+            width: "90%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+
+            <TextInput
+              theme={theme}
+              style={styles.input}
+              onChangeText={handleChange("clinicName")}
+              onBlur={handleBlur("clinicName")}
+              value={values.clinicName}
+              placeholder={"Clinic Name"}
+            />
+            {(errors.clinicName && touched.clinicName) &&
+            <Text style={styles.errorText}>{errors.clinicName}</Text>
+            }
+            <TextInput
+              theme={theme}
+              style={styles.input}
+              onChangeText={handleChange("ClinicCoordinates")}
+              onBlur={handleBlur("ClinicCoordinates")}
+              value={values.ClinicCoordinates}
+              placeholder={"Clinic ID"}
+            />
+            {(errors.ClinicCoordinates && touched.ClinicCoordinates) &&
+            <Text style={styles.errorText}>{errors.ClinicCoordinates}</Text>}
 
 
-    <Formik
-      validationSchema={loginValidationSchema}
-      initialValues={{
-        clinicName: "",
-        AddressOfClinic: "",
-        ClinicCoordinates: "",
-        phoneNo: "",
-        vaccineCount: "",
-      }}
-      onSubmit={values => {
-        console.log(values);
-        const jsonData = JSON.stringify(values);
+            <TextInput
+              theme={theme}
+              style={styles.input}
+              onChangeText={handleChange("phoneNo")}
+              onBlur={handleBlur("phoneNo")}
+              value={values.phoneNo}
+              placeholder={"Phone No."}
+            />
+            {(errors.phoneNo && touched.phoneNo) &&
 
-      }}
+            <Text style={styles.errorText}>{errors.phoneNo}</Text>}
+            <GooglePlacesAutocomplete
+              onPress={(data, details = null) => {
+                // 'details' is provided when fetchDetails = true
+                console.log(data, details);
+              }}
+              placeholder="Enter Location"
+              minLength={2}
+              autoFocus={false}
+              returnKeyType={"default"}
+              fetchDetails={true}
+              renderDescription={row => row.description}
+              listViewDisplayed='auto'
+             onNotFound={(err) => {
+                console.log("NOT_FOUND Google PLace", err);
+              }}
+              onTimeout={() => {
+                console.log("Time OUT Google PLace");
+              }}
+              timeout={5000}
+              styles={{
+                textInputContainer: {
+                  backgroundColor: planted_colors.OFF_WHITE,
+                  width: "90%",
+                  marginTop: 10,
 
-    >
-      {({
-          handleChange, handleBlur, handleSubmit, values, errors,
-          touched,
-          isValid,
-        }) => (
-        <View style={{
-          height: "100%",
-          width: "90%",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-          <Text style={{
+                },
+                textInput: {
+                  backgroundColor: planted_colors.LIGHT_BLUE,
+                  height: 42,
+                  width: "100%",
+                  color: "black",
 
-            color: planted_colors.STRONG_YELLOW,
-          }}>Fill The Form Up </Text>
-          <TextInput
-            theme={theme}
-            style={styles.input}
-            onChangeText={handleChange("clinicName")}
-            onBlur={handleBlur("clinicName")}
-            value={values.clinicName}
-            placeholder={"Email ID"}
-            keyboardType="email-address"
-          />
-          {(errors.clinicName && touched.clinicName) &&
-          <Text style={styles.errorText}>{errors.clinicName}</Text>
-          }
-          <GooglePlacesAutocomplete
-            onPress={(data, details = null) => {
-              // 'details' is provided when fetchDetails = true
-              console.log(data, details);
-            }}
-            placeholder='Enter Location'
-            minLength={2}
-            autoFocus={false}
-            returnKeyType={'default'}
-            fetchDetails={true}
-            styles={{
-              textInputContainer: {
-                backgroundColor: planted_colors.OFF_WHITE,
-                width:"90%",
-                height:"100%"
-              },
-              textInput: {
-                height: 34,
-                width:"100%",
-                color: 'black',
-                fontSize: 16,
-              },
-              predefinedPlacesDescription: {
-                color: '#1faadb',
-              },
-            }}
-            query={{
-              key: 'AIzaSyD09ZcG7fHZltsAOsKjxq5Eww4xEIfXZNc',
-              language: 'en',
-              types:`(cities)`
-            }}
-          />
-          <TextInput
-            theme={theme}
-            style={styles.input}
-            onChangeText={handleChange("AddressOfClinic")}
-            onBlur={handleBlur("AddressOfClinic")}
-            value={values.AddressOfClinic}
-            placeholder={"Address Of Clinic"}
-            secureTextEntry
-          />
-          {(errors.AddressOfClinic && touched.AddressOfClinic) &&
-          <Text style={styles.errorText}>{errors.AddressOfClinic}</Text>
-          }
-          <TextInput
-            theme={theme}
-            style={styles.input}
-            onChangeText={handleChange("ClinicCoordinates")}
-            onBlur={handleBlur("ClinicCoordinates")}
-            value={values.ClinicCoordinates}
-            placeholder={"Aadhar Card"}
-          />
-          {(errors.ClinicCoordinates && touched.ClinicCoordinates) &&
-          <Text style={styles.errorText}>{errors.ClinicCoordinates}</Text>}
+                  fontSize: 16,
+                },
 
+              }}
+              query={{
+                key: "AIzaSyD09ZcG7fHZltsAOsKjxq5Eww4xEIfXZNc",
+                language: "en",
+              }}
 
-          <TextInput
-            theme={theme}
-            style={styles.input}
-            onChangeText={handleChange("phoneNo")}
-            onBlur={handleBlur("phoneNo")}
-            value={values.phoneNo}
-            placeholder={"Phone No."}
-          />
-          {(errors.phoneNo && touched.phoneNo) &&
-          <Text style={styles.errorText}>{errors.phoneNo}</Text>}
-          <Button theme={theme} style={{
-            marginTop: 20,
+            />
+            <Button theme={theme} style={{
+              marginTop: 20,
 
-          }} onPress={handleSubmit} title="Submit" />
-        </View>
-      )}
-    </Formik>
-  </View>
+            }} onPress={handleSubmit} title="Submit" />
+          </View>
+        )}
+      </Formik>
+
+    </View>
+  </ScrollView>
 );
 
 
@@ -154,7 +172,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-
+    marginTop: 200,
     backgroundColor: planted_colors.OFF_WHITE,
   },
   input:
@@ -179,6 +197,13 @@ const styles = StyleSheet.create({
     marginLeft: 20,
 
   },
+  SplashScreen_ChildView:
+    {
+      width:"100%",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center"
+    },
 
 
 });
