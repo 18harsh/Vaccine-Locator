e401e5699d0c0933b5c6a86d49de86cd008073f4
reuben21 <reuben211999@icloud.com>
2021-03-21 18:25:59 +0530

    Updated Time Slot Management

--- vaccine-locator-backend/src/controller/timeController.js
+++ vaccine-locator-backend/src/controller/timeController.js
@@ -1,5 +1,6 @@
 const timeSlotsModel = require('../models/timeSlots');
 const clinicModel = require('../models/clinicCenter');
+const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {validationResult} = require("express-validator");
@@ -24,7 +25,7 @@ exports.addTimeSlots = (req, res, next) => {
                     eventDate: eventDate,
                     eventTiming: {
                         startTime: start_time,
-                        endTime:end_time,
+                        endTime: end_time,
                         allottedTo: [],
                         allotmentLimit: received_count
                     },
@@ -55,13 +56,13 @@ exports.addTimeSlots = (req, res, next) => {
                 // console.log(result.eventDate[i])
                 arrayOfEventDate.push(String(result.eventDate[i].eventDate.toISOString()));
             }
-            console.log(arrayOfEventDate,eventDate)
+            console.log(arrayOfEventDate, eventDate)
             if (arrayOfEventDate.includes(eventDate)) {
 
                 result.eventDate[arrayOfEventDate.indexOf(eventDate)].eventTiming.push({
                     allotmentLimit: received_count,
                     startTime: start_time,
-                    endTime:end_time
+                    endTime: end_time
                 })
                 result.save()
             } else {
@@ -69,7 +70,7 @@ exports.addTimeSlots = (req, res, next) => {
                     eventDate: eventDate,
                     eventTiming: {
                         startTime: start_time,
-                        endTime:end_time,
+                        endTime: end_time,
                         allottedTo: [],
                         allotmentLimit: received_count
                     },
@@ -89,18 +90,55 @@ exports.addTimeSlots = (req, res, next) => {
 exports.fetchSlotsForClinic = (req, res, next) => {
     const clinciObjectId = req.body.clinicObjectId;
 
-    clinicModel.findById(clinciObjectId).then((clinic) => {
-        if (!clinic.timeSlotId) {
-            res.send({"message":"No Slots"})
-        }
-        timeSlotsModel.findById(clinic.timeSlotId).then(result =>{
-            res.send(result.eventDate)
-        })
+    // clinicModel.findById(clinciObjectId)
+    //     .sort({'eventDate.evenDate':-1})
+    //     .exec().
+    // then((clinic) => {
+    //     if (!clinic.timeSlotId) {
+    //         res.send({"message":"No Slots"})
+    //     }
+    //     timeSlotsModel.findById(clinic.timeSlotId).then(result =>{
+    //         res.send(result.eventDate)
+    //     })
+    //
+    // }).then(result => {
+    //     console.log(result)
+    //
+    // })
 
-    }).then(result => {
+    clinicModel.findById(clinciObjectId)
+        .then((clinic) => {
+            if (!clinic.timeSlotId) {
+                res.send({"message": "No Slots"})
+            }
+
+            timeSlotsModel.findById(clinic.timeSlotId)
+                // .sort({'eventDate.evenDate': -1}).exec()
+                .then(result => {
+                    const sortedActivities = result.eventDate
+                        .slice()
+                        .sort((a, b) => a.eventDate - b.eventDate)
+                    res.send(sortedActivities)
+                })
+
+        }).then(result => {
         console.log(result)
 
     })
+
+    // timeSlotsModel.findOneAndUpdate({_id: clinic.timeSlotId},
+    //     {
+    //         $push: {
+    //             'eventDate.$[comment].eventTiming.$[reply].allottedTo': patientObjectId,
+    //
+    //         },
+    //         $set: {
+    //             'eventDate.$[comment].eventTiming.$[reply].allotmentLimit': Limit - 1,
+    //         }
+    //     }, {
+    //         arrayFilters: [{'comment.eventDate': date}, {'reply._id': timeSlotId}],
+    //         new: true
+    //     }
 
 
 }
